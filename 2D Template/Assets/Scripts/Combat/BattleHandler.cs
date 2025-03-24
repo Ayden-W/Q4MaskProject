1b5107d01b8d248f399578c9c0f12e4e9e409fa4
MUKILTEO\1722815 <1722815@mukilteo.wednet.edu>
2025-03-24 13:18:04 -0700

    turn based combat Attack

--- 2D Template/Assets/Scripts/Combat/BattleHandler.cs
+++ 2D Template/Assets/Scripts/Combat/BattleHandler.cs
@@ -3,34 +3,64 @@ using UnityEngine;
 public class BattleHandler : MonoBehaviour
 {
     //Character spawns useing prefabs
-    [SerializeField] private Transform pfCharacter;
-    [SerializeField] private Transform pfEnemy;
+    [SerializeField] private CharacterBattle pfCharacter;
+    [SerializeField] private CharacterBattle pfEnemy;
     public int EnemyspawnX = 4;
     public int EnemyspawnY = 0;
     public int PlayerspawnX = -4;
     public int PlayerspawnY = 0;
+
+    private CharacterBattle playerCharacterBattle;
+    private CharacterBattle enemyCharacterBattle;
+    private CharacterBattle Attack;
+    private State state;
+
+    private enum State
+    {
+        WaitingForPlayer,
+        Busy
+    }
+    
     private void Start()
     {
         //Instantiate(pfCharacter, new Vector3(PlayerspawnX, PlayerspawnY), Quaternion.identity);
         //Instantiate(pfEnemy, new Vector3(EnemyspawnX, EnemyspawnY), Quaternion.identity); 
-        spawnCharacter(true);
-        spawnCharacter(false);
+        
+            playerCharacterBattle = SpawnCharacter(true);
+            enemyCharacterBattle = SpawnCharacter(false);
+            state = State.WaitingForPlayer;
+        
     }
-        private void spawnCharacter(bool isPlayerTeam)
+
+    private void Update()
+    {
+        if (state == State.WaitingForPlayer)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                state = State.Busy;
+                playerCharacterBattle.Attack(enemyCharacterBattle, () =>
+                {
+                    state = State.WaitingForPlayer;
+                });
+            }
+        }
+    }
+    private CharacterBattle SpawnCharacter(bool isPlayerTeam)
     {
         Vector3 position;
         if (isPlayerTeam)
         {
             position = new Vector3(PlayerspawnX, PlayerspawnY);
-
+            return Instantiate(pfCharacter, new Vector3(PlayerspawnX, PlayerspawnY), Quaternion.identity);
         }
         else
         {
             position = new Vector3(EnemyspawnX, EnemyspawnY);
+            return Instantiate(pfEnemy, new Vector3(EnemyspawnX, EnemyspawnY), Quaternion.identity);
         }
-        Instantiate(pfEnemy, new Vector3(EnemyspawnX, EnemyspawnY), Quaternion.identity);
-        Instantiate(pfCharacter, new Vector3(PlayerspawnX, PlayerspawnY), Quaternion.identity);
+        
+        
     }
 }
-    
 
--- 2D Template/Assets/Scripts/Combat/CharacterBattle.cs
+++ 2D Template/Assets/Scripts/Combat/CharacterBattle.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class CharacterBattle : MonoBehaviour
@@ -6,7 +7,30 @@ public class CharacterBattle : MonoBehaviour
     {
         if (isPlayerTeam)
         {
+            // set Anim and Texture
 
+            //characterBase.
+            //characterBase.GetMaterial().mainTexture=
         }
+        else 
+        {
+            // enemy sprite and anim 
+            //7min in video https://www.youtube.com/watch?v=0QU0yV0CYT4&t=413s
+
+            //characterBase.();
+            //characterBase.GetMaterial().mainTexture = BattleHandler.getInstance(). EnemySpritesheet
+        }
+    }
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+    public void Attack(CharacterBattle targetCharacterBattle, Action onAttackComplete)
+    {
+        Vector3 attackDir = (targetCharacterBattle.GetPosition() - GetPosition()).normalized;
+        //CharacterBase.PlayAnimAttack(attackDir, null, () => {
+        //characterBase.PlayAnimIdle(attackDir);
+        Debug.Log("Attack");
+        //});
     }
 }
