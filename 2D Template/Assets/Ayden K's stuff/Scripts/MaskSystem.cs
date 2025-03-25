58341af811cf58c7122160c304489e22ea3fe8f3
MUKILTEO\1722902 <1722902@mukilteo.wednet.edu>
2025-03-25 12:04:47 -0700

    added some masks and started on status effects but ill probably work on them later on

--- 2D Template/Assets/Ayden K's stuff/Scripts/MaskSystem.cs
+++ 2D Template/Assets/Ayden K's stuff/Scripts/MaskSystem.cs
@@ -5,6 +5,7 @@ public abstract class MaskSystem : ScriptableObject
     public string ID;
     public string itemName;
     [TextArea] public string Description;
+    public bool Discardable;
 
     public abstract void PassiveAbility();
     
