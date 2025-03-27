d2e00d29a5fe9473916e19c6369f4d11da384369
MUKILTEO\1722902 <1722902@mukilteo.wednet.edu>
2025-03-27 12:33:54 -0700

    made a partially working healing system

--- 2D Template/Assets/Ayden K's stuff/Scripts/Playerscript.cs
+++ 2D Template/Assets/Ayden K's stuff/Scripts/Playerscript.cs
@@ -1,16 +1,18 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Playerscript : MonoBehaviour
 {
-    public float HealthPool;
+    public float CurrentHealth;
     public float MaxHealth;
     public float TradeBeads;
     public List<ScriptableObject> Inventory;
     public MaskSystem CurrentMask;
     public float Damage;
     public float Defence;
+    public float Healing = 15;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,5 +24,19 @@ public class Playerscript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void healing()
+    {
+        if (CurrentHealth <= MaxHealth)
+        {
+            CurrentHealth += Healing;
+            Debug.Log("healing");
+        }
+        if (CurrentHealth > MaxHealth)
+        {
+            CurrentHealth = MaxHealth;
+            Debug.Log("max health");
+        }
     }
 }
