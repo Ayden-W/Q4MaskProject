4adcd6e1c70b7db6be99ddec0e6ac8c0a08f1ac2
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2025-03-31 13:26:03 -0700

    Grid_Testing, This is a pain in the axis

---
+++ 2D Template/Assets/Ethan's Stuff/Scripts/GridManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Grid))]
+[RequireComponent(typeof(Tilemap))]
+public class GridManager : MonoBehaviour
+{
+    Tilemap tilemap;
+    Grid_Test grid;
+    [SerializeField] TileBase tilebase;
+    [SerializeField] TileBase tilebase2;
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        grid = GetComponent<Grid_Test>();
+        grid.Init(10, 8);
+        grid.Set(10, 0, true);
+        UpdateTileMap();
+    }
+
+    void UpdateTileMap()
+    {
+        for (int x = 0; x < grid.length; x++)
+        {
+            for (int y = 0; y < grid.height; y++)
+            {
+                if(grid.Get(x, y))
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tilebase);
+                }
+                else
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tilebase2);
+                }
+                
+            }
+        }
+    }
+   
+}
---
+++ 2D Template/Assets/Ethan's Stuff/Scripts/Grid_Test.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Grid_Test : MonoBehaviour
+{
+   public int height;
+   public int length;
+
+    bool[,] grid;
+
+    public void Init(int length, int height)
+    {
+        grid = new bool[length, height];
+        this.length = length;
+        this.height = height;
+    }
+
+    public void Set(int x, int y, bool to)
+    {
+        if(CheckPosition(x,y) == false) 
+        {
+            Debug.LogWarning("Ha, Idiot" + x.ToString() + ": " + y.ToString());
+            return; 
+        }
+        grid[x, y] = to;
+    }
+
+    public bool Get(int x, int y)
+    {
+        if (CheckPosition(x, y) == false)
+        {
+            Debug.LogWarning("Ha, Idiot" + x.ToString() + ": " + y.ToString());
+            
+            return false; 
+        }
+        return grid[x, y];
+    }
+
+    public bool CheckPosition(int x, int y)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+
+        if (y < 0)
+        {
+            return false;
+        }
+        return true;
+    }
+}
