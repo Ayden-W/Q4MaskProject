88ed725cbe8e7b3e90b3040110b2579fd742aa59
MUKILTEO\1722902 <1722902@mukilteo.wednet.edu>
2025-04-14 13:20:04 -0700

    got a mask to work

--- 2D Template/Assets/Ayden K's stuff/Scripts/Playerscript.cs
+++ 2D Template/Assets/Ayden K's stuff/Scripts/Playerscript.cs
@@ -8,22 +8,25 @@ public class Playerscript : MonoBehaviour
     public float CurrentHealth;
     public float MaxHealth;
     public float TradeBeads;
-    public List<ScriptableObject> Inventory;
-    public MaskSystem CurrentMask;
+    public List<MaskSystem> Inventory;
     public float Damage;
     public float Defence;
     public float Healing = 15;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        CurrentMask.PassiveAbility();
-
+        foreach (var item in Inventory)
+        {
+            Debug.Log(item.name);
+            item.OnEquip(this);
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Don't forget to call a Mask's Equip and Unequip functions when they enter and leave your inventory.
     }
 
     public void healing()
