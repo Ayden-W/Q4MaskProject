e71edf17c635eb99af28cfabb58cd650316fbe58
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2025-04-15 13:17:51 -0700

    NODES N LINES HURT MY EYES

--- 2D Template/Assets/Ethan's Stuff/Scripts/MapGrid.cs
+++ 2D Template/Assets/Ethan's Stuff/Scripts/MapGrid.cs
@@ -5,20 +5,33 @@ public class MapGrid : MonoBehaviour
 
     public GameObject node;
     private int randomInt;
+    private int randomInt2;
     private int nodePosition;
     private Vector3 randomPosition;
+    private Vector3 randomPosition2;
     private float originalX;
  
-
+    
     private void Awake()
     {
+       
 
         for (int i = 0; i < 10; i++)
         {
-
-            randomInt = Random.Range(1, 13);
-            randomPosition = new Vector2(randomPosition.x + 1, randomInt);
+            
+            randomInt = Random.Range(-4, 4);
+            randomInt2 = Random.Range(-4, 4);// Minimum and Maximum of Y 
+            randomPosition = new Vector2(-2 + randomPosition.x + 1, randomInt);
+            randomPosition2 = new Vector2(-2 + randomPosition2.x + 1, randomInt2);
             Instantiate(node, randomPosition, node.transform.rotation);
+            Instantiate(node, randomPosition2, node.transform.rotation);
+
+            /*LineRenderer lr = FindFirstObjectByType<LineRenderer>();*/ // Grabs the LineRenderer
+            LineRenderer lr = FindFirstObjectByType<LineRenderer>();
+
+            lr.positionCount++;   //Sets line for LineRenderer
+            lr.SetPosition(lr.positionCount-1, randomPosition);
+            
         }
     }
 
---
+++ 2D Template/Assets/Ethan's Stuff/Scripts/lineController.cs
@@ -0,0 +1,29 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class lineController : MonoBehaviour
+{
+
+    private LineRenderer lr;
+    private Transform[] points;
+    
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        
+    }
+
+    public void SetUpLine(Transform[] points)
+    {
+        lr.positionCount = points.Length;
+        this.points = points;
+    }
+    private void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            lr.SetPosition(i , points[i].position);
+        }
+    }
+}
