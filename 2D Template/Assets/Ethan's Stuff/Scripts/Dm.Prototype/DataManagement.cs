d2669825b0406ca77be5346a789f480eb5fd8d5b
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2025-04-23 13:06:27 -0700

    Some stuff gone but some isn't so eh :D Raining Pianos

---
+++ 2D Template/Assets/Ethan's Stuff/Scripts/Dm.Prototype/DataManagement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class DataManagement : MonoBehaviour
+{
+    // This is for later use no touchie.......
+    // ...
+    // No
+    // Uh still no
+    // Hey uhm the thing is still no
+    // The no is still well the no
+    // Go elsewhere till it's put in.
+    // It as in the script.
+    // -._.-
+}
