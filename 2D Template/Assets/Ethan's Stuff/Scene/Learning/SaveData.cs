29621aa2283368d8336baf3da79d5b62e59fa8a8
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2025-05-01 13:33:06 -0700

    SaveData before bell rung

---
+++ 2D Template/Assets/Ethan's Stuff/Scene/Learning/SaveData.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+[System.Serializable]
+public struct SaveData
+{
+    public Vector2 position;
+    public int currency;
+    public float health;
+    public string name;
+    public bool isalive;
+
+    //Holds variables that will be saved
+}
