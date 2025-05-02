73299bc4ff205f236cf43bb7c0c93b34ee02320b
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2025-05-02 13:25:27 -0700

    Shope

--- 2D Template/Assets/Ethan's Stuff/Scripts/SceneChange.cs
+++ 2D Template/Assets/Ethan's Stuff/Scripts/SceneChange.cs
@@ -8,6 +8,6 @@ public class SceneChange : MonoBehaviour
     public void OnClick()
     {  
         // This is only for my Shope scene, because yes. Unless of course you have a button that were to change the scene to the GridTest scene.
-        SceneManager.LoadScene("GridTest");
+        SceneManager.LoadScene("Grid Test");
     }
 }
